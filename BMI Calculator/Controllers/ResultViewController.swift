95be633c303476d4e6acced3d71e917ec14b8346
Sergey Philippov <quadratte@gmail.com>
2025-05-23 22:26:57 +0700

    Done. Where is my salary??

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -1,18 +1,21 @@
- import UIKit
+import UIKit
 
 class ResultViewController: UIViewController {
 
   var bmiValue: String?
+  var advice: String?
+  var color: UIColor?
   
   @IBOutlet var bmiLabel: UILabel!
   @IBOutlet weak var adviceLabel: UILabel!
   
-  
   override func viewDidLoad() {
-        super.viewDidLoad()
-
+    super.viewDidLoad()
+    
     bmiLabel.text = bmiValue
-    }
+    adviceLabel.text = advice
+    view.backgroundColor = color
+  }
     
   @IBAction func recalculatePressed(_ sender: UIButton) {
     self.dismiss(animated: true)
--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -2,16 +2,32 @@ import UIKit
 
 struct CalculatorBrain {
   
-  var bmi: Float?
+  var bmi: BMI?
+  var advice: String?
   
   func getBMIValue() -> String {
-    let convertedBMI = bmi ?? 0.0
+    let convertedBMI = bmi?.value ?? 0.0
     return String(format: "%.1f", convertedBMI)
   }
   
+  mutating func getAdvice() -> String {
+    bmi?.advice ?? "Some text should be here"
+  }
+  
+  func getColor() -> UIColor {
+    bmi?.color ?? UIColor.white
+  }
   
   mutating func calculateBMI(height: Float, weight: Float) {
-    bmi = weight / (height * height)
+    let bmiValue = weight / (height * height)
+    
+    if bmiValue < 18.5 {
+      bmi = BMI(value: bmiValue, advice: "Eat more pies", color: .cyan)
+    } else if bmiValue < 24.9 {
+      bmi = BMI(value: bmiValue, advice: "Ok", color: .green)
+    } else {
+      bmi = BMI(value: bmiValue, advice: "Eat less snacks", color: .red)
+    }
   }
   
 }
